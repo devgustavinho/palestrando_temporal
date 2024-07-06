90af53a19976d27c7db5e05d99ef39e071dad1a9
Gustavo Carneiro <gustavocarneiro.a@gmail.com>
2024-07-06 00:23:44 -0300

    feat: adding secret workflow

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { temporalClient } from './temporal-client.js';
+import { WorkflowIdReusePolicy } from '@temporalio/client';
 const app = express();
 const port = 3001;
 
@@ -23,6 +24,54 @@ app.get('/kwanzas-matematicas', async (req, res) => {
     res.json({ reais: `R$ ${valor.toFixed(2)}` });
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/sorteio', async (req, res) => {
+    if (!req.query.nome) {
+        res.json({
+            epa: "Epa, adicione ?nome=MeuNomeAqui para participar do sorteio",
+        });
+        return;
+    }
+    const temporal = await temporalClient();
+    try {
+        await temporal.workflow.signalWithStart('sorteioLivroWorkflow', {
+            taskQueue: 'banco',
+            workflowId: 'sorteio-livro-valendo2',
+            signal: 'tentativa',
+            signalArgs: [req.query.nome],
+            workflowIdReusePolicy: WorkflowIdReusePolicy.WORKFLOW_ID_REUSE_POLICY_REJECT_DUPLICATE,
+        });
+    
+        res.json({
+            aiSim: "Tentativa registrada",
+        })
+    } catch {
+        try {
+            const workflowHandler =  temporal.workflow.getHandle('sorteio-livro-valendo2');
+            const vencedor = await workflowHandler.query('vencedor');
+            res.json({
+                opa: "Workflow de sorteio encerrado!",
+                vencedor,
+            })
+        } catch {
+            res.json({ opa: "Workflow de sorteio encerrado!", })
+        }
+    }
+})
 app.listen(port, () => {
     console.log(`[APP] Servidor rodando em http://localhost:${port}`);
 });
--- ts_worker/src/workflows.ts
+++ ts_worker/src/workflows.ts
@@ -1,4 +1,4 @@
-import { executeChild, proxyActivities } from '@temporalio/workflow';
+import { condition, defineQuery, defineSignal, executeChild, proxyActivities, setHandler } from '@temporalio/workflow';
 import type * as activities from './activities';
 enum Moedas  {
   REAL = "BRL", 
@@ -33,4 +33,73 @@ export async function valorEmReaisViaAPI(): Promise<number> {
   const bolsaValoresKwanzas = await buscar_dados_api(Moedas.KWANZAS, Moedas.REAL);
   const valorEmRais: number = await calcular_matematica_avancada(bolsaValoresKwanzas.value, valor);
   return valorEmRais;
+}
+
+/* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Workflow secreto
+*/
+const tentativasGeraisQuery = defineQuery('tentativas_gerais');
+const tentativaSignal = defineSignal<[string]>('tentativa');
+const vencedorQuery = defineQuery('vencedor');
+export async function sorteioLivroWorkflow() {
+  const tentativas: { [key: string]: number } = {};
+  const numeroCorreto = aleatorio(1, 1000);
+  const vencedor = {
+    nome: null,
+    tentativa: null,
+    tipoDeVitoria: "sorteio final"
+  };
+
+  setHandler(tentativaSignal, (tentante: string) => {
+    tentativas[tentante] = tentativas[tentante] ? tentativas[tentante] + 1 : 1;
+    const numeroDaTentativa = aleatorio(1, 1000);
+    if (!vencedor.nome && numeroDaTentativa == numeroCorreto) {
+      Object.assign(vencedor, { nome: tentante, tentativa: tentativas[tentante], tipoDeVitoria: "pura sorte" })
+    }
+  })
+  setHandler(tentativasGeraisQuery, () => tentativas);
+  setHandler(vencedorQuery, () => vencedor);
+
+  const condicaoAtingida = await condition(() => !!vencedor?.nome, '5 minutes');
+
+  if(!condicaoAtingida) {
+    const vencedorIndex = aleatorio(0, Object.keys(tentativas).length - 1);
+    const tentante = Object.keys(tentativas)[vencedorIndex];
+    Object.assign(vencedor, { nome: tentante, tentativa: tentativas[tentante] });
+  } else {
+    console.log(`Número ${numeroCorreto} encontrado`);
+  }
+
+  return vencedor;
+}
+
+function aleatorio(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
